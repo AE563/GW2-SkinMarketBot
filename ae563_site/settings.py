e36a2e3c7a4166f84ca086acc9f7461a12682039
Ivan Zhilnikov <feliksesae@gmail.com>
2023-09-20 16:01:03 +0400

    static fix

--- ae563_site/settings.py
+++ ae563_site/settings.py
@@ -14,12 +14,12 @@ import os.path
 from pathlib import Path
 
 from django.contrib import staticfiles
+from django.core.checks import templates
 
 # from SECRET_KEY import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -34,7 +34,6 @@ ALLOWED_HOSTS = ['127.0.0.1',
                  'www.ae563.ru',
                  'ae563.ru',
                  ]
-
 
 # Application definition
 
@@ -65,8 +64,7 @@ ROOT_URLCONF = 'ae563_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -81,7 +79,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'ae563_site.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -91,7 +88,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -112,7 +108,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -126,14 +121,13 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR, '../rpssl/static')
 # STATICFILES_DIRS = []
-STATIC_URL = '/static/'
+STATIC_URL = 'ae563_site/staticfiles/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'common_assets', 'static'),
 ]
