57f090c37689c9a39d1ab932d33706c89b94535f
Ivan Zhilnikov <feliksesae@gmail.com>
2023-09-23 00:48:57 +0400

    fix2

---
+++ ae563_site/SECRET_KEY.py
@@ -0,0 +1,2 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-rjfbsp#!)@ir!o=%g)ysyl6pbci9c#fys1_#iw7!*!zol+k=45'
---
+++ gw2_tp/tokens.py
@@ -0,0 +1,8 @@
+# Замените 'GW_API_ACCESS_KEY' на ваш настоящий API key Guild Wars 2 account.
+GW_API_ACCESS_KEY = \
+    '44718987-59DC-234C-AEFA-8491368222AF8DD722F7-0A84-4218-B2E8-AD56903D4503'
+
+# Замените 'TG_BOT_TOKEN' на ваш настоящий токен Telegram бота
+TG_BOT_TOKEN = '5759952619:AAGzguveLyOFmiyjHLOVvcZUkl9O-x-gmvQ'
+# Замените 'TG_CHAT_ID' на ваш настоящий токен Telegram чата
+TG_CHAT_ID = -847535892
