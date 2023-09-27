fd492adbd9bb646d3be0c50bc5f3438b06b12df3
Ivan Zhilnikov <feliksesae@gmail.com>
2023-09-27 18:20:16 +0400

    DEBUG = False

--- ae563_site/settings.py
+++ ae563_site/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-rjfbsp#!)@ir!o=%g)ysyl6pbci9c#fys1_#iw7!*!zol+k=45'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1',
                  'www.ae563.ru',
